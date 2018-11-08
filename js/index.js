d7a6d1373da75612ac1faacdf34154b2863dba39
Anastasiia Litvina <al@click-solutions.de>
2018-11-09 00:26:23 +0100

    add layout

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let info = document.getElementsByClassName('info__container')[0];
+function showInfo() {
+    info.classList.add('active');
+}
+
+function hideInfo() {
+    info.classList.remove('active')
+}
